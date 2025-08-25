e8e4ae2350206c2ff12484411979d75d080b2c83
the843 <faikohicham15@gmail.com>
2025-08-25 12:55:17 +0100

    I added functionalty

---
+++ index.js
@@ -0,0 +1,19 @@
+let btn = document.querySelectorAll("button");
+let input = document.querySelector("input");
+
+btn.forEach(button => {
+    button.addEventListener("click", (e) => {
+        const value = e.target.textContent;
+        if (value === "C") {
+            input.value = "";
+        } else if (value === "=") {
+            try {
+                input.value = eval(input.value);
+            } catch {
+                input.value = "Error";
+            }
+        } else {
+            input.value += value;
+        }
+    });
+});
